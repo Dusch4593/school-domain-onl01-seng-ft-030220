5af1b8b8ad3f64a761d9cad5f19a006d8fa4a91f
Brandon Dusch <brandondusch@gmail.com>
2020-03-07 18:30:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,7 @@
-require 'pry'
-
-# code here!
 class School 
-  def initialize(name, roster={})
-    @name = name
-    @roster = roster
-  end
-  
-  def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
-  end
-  
-  def grade(grade_number)
-    @roster[grade_number]
+  def initialize(name)
+    @name = name 
   end
 end
-
 
 school = School.new("Minadeo Elementary School")
